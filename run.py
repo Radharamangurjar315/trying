b840aa045d8308c2ca7bae7c19301e8d94fc1815
Umesh-142 <128925997+Umesh-142@users.noreply.github.com>
2025-08-03 03:00:26 +0530

    try

--- run.py
+++ run.py
@@ -5,16 +5,9 @@
 # if __name__ == "__main__":
 #     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
 import os
-from dotenv import load_dotenv
 import uvicorn
 
-# Load .env file into environment
-load_dotenv()
-
 if __name__ == "__main__":
-    port = int(
-        os.environ.get("PORT") or  # Render
-        os.environ.get("port", 8000)  # Local .env
-    )
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
-
+    # Render always sets PORT environment variable
+    port = int(os.environ.get("PORT"))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
