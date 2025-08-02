2760e14e2e4d1cee170c25446bdfb6126d38d1ee
Umesh-142 <128925997+Umesh-142@users.noreply.github.com>
2025-08-03 02:28:03 +0530

    port change

--- run.py
+++ run.py
@@ -5,8 +5,16 @@
 # if __name__ == "__main__":
 #     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
 import os
+from dotenv import load_dotenv
 import uvicorn
 
+# Load .env file into environment
+load_dotenv()
+
 if __name__ == "__main__":
-    port = int(os.environ.get("port", 8000))  # Default 8000 for local
+    port = int(
+        os.environ.get("port") or  # Render
+        os.environ.get("port", 8000)  # Local .env
+    )
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+
