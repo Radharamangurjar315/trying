8bd49c5d3535ee0e2c92fb4a66218d9e2e93827a
Umesh-142 <128925997+Umesh-142@users.noreply.github.com>
2025-08-03 02:41:33 +0530

    bit change

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ load_dotenv()
 
 if __name__ == "__main__":
     port = int(
-        os.environ.get("port") or  # Render
+        os.environ.get("PORT") or  # Render
         os.environ.get("port", 8000)  # Local .env
     )
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
