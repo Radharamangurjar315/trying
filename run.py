9f04ea493b5fdfaf39a51e64ec26f5afafc586f9
Umesh-142 <128925997+Umesh-142@users.noreply.github.com>
2025-08-03 02:17:33 +0530

    deploy issue resolve

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+# from app.settings import settings
+# port = settings.port
+# import uvicorn
+
+# if __name__ == "__main__":
+#     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=3000, reload=True)
+    port = int(os.environ.get("port", 8000))  # Default 8000 for local
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
